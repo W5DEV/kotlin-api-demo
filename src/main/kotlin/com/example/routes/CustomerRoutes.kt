b473c33865d880e39d039aa6965272b2e66aec25
John Larson <jl@buildmidwestern.com>
2024-01-07 04:43:08 -0600

    added customer routes model and storage

---
+++ src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -0,0 +1,20 @@
+package com.example.routes
+
+import com.example.models.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+            if (customerStorage.isNotEmpty()) {
+                call.respond(customerStorage)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.OK)
+            }
+        }
+    }
+}
