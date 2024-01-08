19d8a1d8b5cd56aae0c29121a8d4ab2a2d51f661
John Larson <jl@buildmidwestern.com>
2024-01-07 21:11:38 -0600

    added customer routes test, added order routes, added order routes test.

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -7,15 +7,14 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
-class ApplicationTest {
+class OrderRouteTests {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
+    fun testGetOrder() = testApplication {
+        val response = client.get("/order/2020-04-06-01")
+        assertEquals(
+            """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 }
